7048f3ebf0728f9ddf3af370b6288248acb8dfd5
Ticoworld <timothyticoworld@gmail.com>
2024-02-20 15:24:31 +0100

    cors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
-app.use(cors());
+app.use(cors())
 app.use(express.json());
 const dotenv = require('dotenv');
 const saltRounds = 10;
